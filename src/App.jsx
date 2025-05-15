15d33ce33742839087435b66c663c4a1055daf6c
andreamaluta <andreamaluta@gmail.com>
2025-05-15 15:41:18 +0200

    [UPD] linked the pages to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import About from './pages/About'
+import Product from './pages/Product'
 
 
 function App() {
@@ -6,7 +10,13 @@ function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' Component={HomePage}></Route>
+          <Route path='/about' Component={About}></Route>
+          <Route path='/product' Component={Product}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
