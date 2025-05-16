11a5b838fa397dafa6672dd134d29f471a165202
andreamaluta <andreamaluta@gmail.com>
2025-05-16 16:18:21 +0200

    [UPD] import and create the route for the Character datail

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import About from './pages/About'
 import Characters from './pages/characters/Characters'
+import CharacterDetail from './pages/characters/CharacterDetail'
 
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
           <Route path='/about' Component={About}></Route>
           <Route path='/actors'>
             <Route index Component={Characters}></Route>
-            {/* <Route path=':id' Component={DetailActor}></Route> */}
+            <Route path=':id' Component={CharacterDetail}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
