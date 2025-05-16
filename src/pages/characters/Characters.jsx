16c26e98ec80caed6f324272eda23edfcf241a50
andreamaluta <andreamaluta@gmail.com>
2025-05-16 16:29:13 +0200

    [UPD] made the card a link

--- src/pages/characters/Characters.jsx
+++ src/pages/characters/Characters.jsx
@@ -2,6 +2,7 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import Navbar from '../../components/Navbar'
+import { Link } from 'react-router-dom';
 
 const Actors = () => {
 
@@ -21,21 +22,23 @@ const Actors = () => {
                 <div className="row">
                     {character.map((char) => (
                         <div key={char.id} className="col-12 mb-3">
-                            <div className="card d-flex flex-row">
-                                <div className="card-img">
-                                    <img src={char.image} alt="" />
+                            <Link to={`/characters/${char.id}`} className='text-decoration-none' >
+                                <div className="card d-flex flex-row">
+                                    <div className="card-img">
+                                        <img src={char.image} alt="" />
+                                    </div>
+                                    <div className="card-body ">
+                                        <div className="name mb-3"><h2>{char.name}</h2></div>
+                                        <div className="data mb-3"><h4>{char.status}</h4></div>
+                                        <div className="nation mb-3"><h4>{char.species}</h4></div>
+                                        <div className="bio mb-3">{char.origin.name}</div>
+                                    </div>
                                 </div>
-                                <div className="card-body ">
-                                    <div className="name mb-3"><h2>{char.name}</h2></div>
-                                    <div className="data mb-3"><h4>{char.status}</h4></div>
-                                    <div className="nation mb-3"><h4>{char.species}</h4></div>
-                                    <div className="bio mb-3">{char.origin.name}</div>
-                                </div>
-                            </div>
+                            </Link>
                         </div>
                     ))}
                 </div>
-            </div>
+            </div >
 
         </>
     )
